a29fc0269dd9ccf969b4b84c7ddef84fe087f17b
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-11-21 10:08:05 +0300

    pipenv file

--- server/src/app.py
+++ server/src/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from model import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@localhost:5432/postgres"
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello():
