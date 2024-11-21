44fe6b6b8218abaa35135bd714e5a9af494435f8
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-11-21 12:38:50 +0300

    error handling

--- server/src/app.py
+++ server/src/app.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from model import db
+from exceptions import AppException
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@localhost:5432/postgres"
 
+@app.errorhandler(AppException)
+def handle_app_exception(error):
+    response = jsonify({"error": error.message})
+    response.status_code = error.status_code
+    return response
+
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
 
 if __name__ == "__main__":
--- server/src/model.py
+++ server/src/model.py
@@ -7,3 +7,9 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str]
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    content: Mapped[str]
