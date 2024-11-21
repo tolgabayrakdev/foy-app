b966f1a8d7038ea259e7e8460954286524d40601
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-11-21 10:15:33 +0300

    s

---
+++ server/src/controller/auth_controller.py
@@ -0,0 +1,2 @@
+from flask import blueprints, request, jsonify
+
--- server/src/model.py
+++ server/src/model.py
@@ -1,4 +1,3 @@
-from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_sqlalchemy import SQLAlchemy
 
