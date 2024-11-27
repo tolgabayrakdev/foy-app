36a85d8cb32e1fd4090dc9eeafe27e63150f85d8
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-11-27 14:28:43 +0300

    s

--- server/src/controller/user_controller.py
+++ server/src/controller/user_controller.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, request, jsonify
 from ..service.user_service import UserService
+from ..decorator.token_required import token_required
 
 user_controller = Blueprint("user_controller", __name__)
 
 @user_controller.route("/update-user", methods=["PUT"])
+@token_required
 def update_user():
     data = request.get_json()
-    user = UserService.update_user(data["id"], username=data["username"], email=data["email"])
+    user = UserService.update_user(request.user["user_id"], **data)
     return jsonify({"id": user.id, "username": user.username, "email": user.email}), 200
 
+
+@user_controller.route("/change-password", methods=["POST"])
+@token_required
+def change_password():
+    data = request.get_json()
+    user = UserService.change_password(request.user["user_id"], data["new_password"])
+    return jsonify({"id": user.id, "username": user.username, "email": user.email}), 200
